60f263b05b7cbc33ca584bef7cffeaba1e00e882
Lee Zong Yu <65748142+marvenlee2486@users.noreply.github.com>
2024-04-18 14:18:10 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
     
     cout << "Enter your password: ";
     cin >> password; 
-    // 1234 is our password AGAIN
+    // 1234 is our password lo
     if(password == 1234) {
         cout << "Access granted.";   
     } else {
