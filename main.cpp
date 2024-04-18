d3ed6fb4c19e8546dbcd4b7f6b2388bce0ce224d
Lee Zong Yu <65748142+marvenlee2486@users.noreply.github.com>
2024-04-18 14:14:24 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,9 +4,7 @@ using namespace std;
 int main() {
     int password;
     cout << "Enter your password: ";
-    cin >> password;    
-    // This is just anyhow one
-    // MORE
+    cin >> password; 
     if(password == 1234) {
         cout << "Access granted.";   
     } else {
